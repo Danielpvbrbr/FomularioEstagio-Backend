34f997f69ec99b7fd18e4ef13c9e7a86495b1859
Danielpvbrbr <dntechpvbr@gmail.com>
2021-09-10 14:42:35 -0300

    subindo projeto

--- index.js
+++ index.js
@@ -13,39 +13,123 @@ app.get('/', (req, res) => {
 });
 
 app.post('/api/postcontt', (req, res) => {
-    const nome = req.body.nome;
-    const sexo = req.body.sexo;
-    const dataCadastro = req.body.dataCadastro;
-    const cidade = req.body.cidade;
-    
-    connection.query('INSERT INTO contatostb (nome, sexo, dataCadastro, cidade) VALUES (?,?,?,?)',[nome, sexo, dataCadastro, cidade],
-    (err, result) => {
-        if(err){
+    const newNome = req.body.newNome;
+    const newSexo = req.body.newSexo;
+    const newCidade = req.body.newCidade;
+    const newData = req.body.newData;;
+
+    connection.query('INSERT INTO contatostb (nome, sexo, dataReg, cidade) VALUES (?,?,?,?)', [newNome, newSexo, newData, newCidade],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        })
+});
+
+app.get('/api/getcontt', (req, res) => {
+    connection.query('SELECT * FROM contatostb', (err, result) => {
+        if (err) {
             console.log(err)
-        }else {
+        } else {
             res.send(result)
         }
     })
 });
 
-app.get('/api/getcontt',(req, res) => {
-    connection.query('SELECT * FROM contatostb',(err, result) => { 
-        if(err){
+app.delete('/api/delID/:id', (req, res) => {
+    const id = req.params.id;
+    connection.query(`DELETE FROM contatostb WHERE id=${id}`,
+        (result, err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        })
+});
+
+// SELECT
+// COUNT(*) AS qtd,
+// sexo,
+// cidade
+// FROM
+// contatostb
+// WHERE
+// cidade = "Bicas" AND MONTH(dataReg) = "09"
+// GROUP BY
+// codcontato
+// SELECT
+//     COUNT(*) AS qtd,
+//     sexo,
+//     cidade,
+//     dataReg
+    
+// FROM
+//     contatostb
+ 
+// GROUP BY
+//     codcontato
+
+//SELECT COUNT(*) AS qtd, sexo, cidade FROM contatostb WHERE cidade ="Juiz de fora" GROUP BY sexo
+
+//SELECT COUNT(*) AS qtd, sexo, cidade FROM contatostb WHERE cidade ="Bicas" GROUP BY sexo
+//UNION ALl
+//SELECT COUNT(*) AS qtd, sexo, cidade FROM contatostb WHERE cidade ="Juiz de fora" GROUP BY sexo
+
+//SELECT dataReg, COUNT(*) AS qtd, sexo, cidade FROM contatostb GROUP BY codcontato
+
+app.get('/api/get/jf', (req, res) => {
+    connection.query('SELECT COUNT(*) AS qtd, sexo, cidade FROM contatostb WHERE cidade ="Juiz de fora" GROUP BY sexo', (err, result) => {
+        if (err) {
             console.log(err)
         } else {
             res.send(result)
         }
     })
 });
-app.get('/api/get',(req, res) => {
-    connection.query('SELECT COUNT(*) as qtd ,sexo, cidade FROM contatostb WHERE cidade="Bicas" group by sexo',(err, result) => { 
-        if(err){
+
+app.get('/api/get/bc', (req, res) => {
+    connection.query('SELECT COUNT(*) AS qtd, sexo, cidade FROM contatostb WHERE cidade = "Bicas" GROUP BY sexo', (err, result) => {
+        if (err) {
             console.log(err)
         } else {
             res.send(result)
         }
     })
+
 });
+
+app.put('/ap/upd', (req, res) => {
+    const id = req.body.id;
+    const newNome = req.body.newNome;
+    const newSexo = req.body.newSexo;
+    const newCidade = req.body.newCidade;
+    const newData = req.body.newData;
+
+    connection.query(`UPDATE contatostb SET nome=?, dataReg=?, sexo=?, cidade=? WHERE codcontato=?`, [newNome, newData, newSexo, newCidade, id],
+        (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                res.send(result)
+            }
+        })
+});
+
+app.delete('/ap/delete/:id', (req, res) => {
+    const id = req.params.id;
+
+    connection.query(`DELETE FROM contatostb WHERE codcontato = ${id}`,
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(result);
+            }
+        })
+})
 
 
 
